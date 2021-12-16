59b8c6cdb4a2037f4e66416d0f0b9d76d202f7e0
Saskia <Saskia_Studer@hotmail.com>
2021-12-16 14:50:09 +0100

    Add files via upload

---
+++ 13-online-data-json/script.js
@@ -0,0 +1,17 @@
+
+persoon = JSON.stringify(persoon);
+var persoon = {
+    Naam: "Zara",
+    Score: 36,
+    "Begin datum": "21 Januari 2016",
+    Vrienden: [
+        "Mika",
+        "Josef",
+        "Maria",
+        "Sumail",
+        "Lotte"],
+    Gekwalificeerd: true,
+};
+
+
+console.log(persoon);
